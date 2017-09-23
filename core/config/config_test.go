7243bbdeccae99256b5585eda3added905f7d992
oif <neowiz@icloud.com>
2017-09-23 11:48:54 +0800

    ADD: test case for config

--- core/config/config_test.go
+++ core/config/config_test.go
@@ -3,10 +3,11 @@ package config
 import (
 	"testing"
 
+	"github.com/oif/apex/core/types"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestConfig(t *testing.T) {
+func TestLoadConfig(t *testing.T) {
 	config := NewConfig()
 	assert.NotPanics(t, func() {
 		config.Load("test.toml").Check()
@@ -16,4 +17,104 @@ func TestConfig(t *testing.T) {
 	assert.Panics(t, func() {
 		config.Load("no_exists.toml")
 	}, "Should panic due to file not exists")
+
+	config = NewConfig()
+	assert.Panics(t, func() {
+		config.Load("test_bad.toml")
+	}, "Should panic due to invalid toml format file given")
+}
+
+func TestUpstreamCheck(t *testing.T) {
+	ups := &Upstream{}
+	assert.Panics(t, func() {
+		ups.Check()
+	}, "Should panic due to primary DNS and alternative DNS are both empty")
+
+	ups.PrimaryDNS = append(ups.PrimaryDNS, &types.Upstream{})
+	assert.Panics(t, func() {
+		ups.Check()
+	}, "Should panic due to primary DNS check fail")
+
+	ups.PrimaryDNS = make([]*types.Upstream, 0)
+
+	ups.AlternativeDNS = append(ups.AlternativeDNS, &types.Upstream{})
+	ups.AlternativeDNS = append(ups.PrimaryDNS, &types.Upstream{})
+	assert.Panics(t, func() {
+		ups.Check()
+	}, "Should panic due to alternative DNS check fail")
+
+	ups.AlternativeDNS[0] = &types.Upstream{
+		Name:     "Google DNS",
+		Address:  "8.8.8.8:53",
+		Protocol: "udp",
+	}
+	assert.NotPanics(t, func() {
+		ups.Check()
+	}, "No panic here")
+}
+
+func TestProxyCheck(t *testing.T) {
+	p := &Proxy{
+		Policy: "",
+	}
+
+	assert.Panics(t, func() {
+		p.Check()
+	}, "Should Panic due to invalid policy")
+
+	p.Policy = types.ProxyActivePolicy
+	assert.Panics(t, func() {
+		p.Check()
+	}, "Should panic due to did not set proxy")
+
+	p.Proxy = &types.Proxy{
+		Protocol: "http",
+	}
+	assert.Panics(t, func() {
+		p.Check()
+	}, "Should panic due to invalid proxy")
+
+	p.Proxy = &types.Proxy{
+		Protocol: "http",
+		Address:  "127.0.0.1:1080",
+	}
+	assert.NotPanics(t, func() {
+		p.Check()
+	}, "No panic here")
+}
+
+func TestCacheCheck(t *testing.T) {
+	c := &Cache{
+		Enable: true,
+		Size:   -1,
+	}
+
+	assert.Panics(t, func() {
+		c.Check()
+	}, "Should panic due to invalid size")
+
+	c.Size = 2
+	assert.NotPanics(t, func() {
+		c.Check()
+	}, "No panic here")
+}
+
+func TestHostsCheck(t *testing.T) {
+	h := &Hosts{
+		Enable: true,
+	}
+
+	assert.Panics(t, func() {
+		h.Check()
+	}, "Should panic due to empty address")
+
+	h.Address = "https://line.cat/hosts"
+	assert.Panics(t, func() {
+		h.Check()
+	}, "Should panic due to update interval < 0")
+
+	h.UpdateInterval = 3600
+	assert.NotPanics(t, func() {
+		h.Check()
+	}, "No panic here")
 }
