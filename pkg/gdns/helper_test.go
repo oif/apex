f0d166d648dbe6b06b39af9225c780564b0f113f
oif <neowiz@icloud.com>
2017-09-24 12:23:04 +0800

    MOD: modify test case

--- pkg/gdns/helper_test.go
+++ pkg/gdns/helper_test.go
@@ -10,13 +10,13 @@ import (
 
 func TestHTTPRequest(t *testing.T) {
 	HTTPClient = nil
-	_, _, err := httpGet(http.MethodGet, "https://baidu.com", nil)
+	_, _, err := httpGet(http.MethodGet, "https://github.com", nil)
 	assert.Error(t, err, "Error due to did not initialize http client")
 
 	HTTPClient = &http.Client{
-		Timeout: 2 * time.Second,
+		Timeout: 5 * time.Second,
 	}
-	_, statusCode, err := httpGet(http.MethodGet, "https://baidu.com", nil)
+	_, statusCode, err := httpGet(http.MethodGet, "https://github.com", nil)
 	assert.Equalf(t, 200, statusCode, "Except status code 200, but %d get", statusCode)
 	assert.NoError(t, err, "Error due to did not initialize http client")
 }
