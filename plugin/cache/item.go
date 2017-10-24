b343f3d80794575216966aa45b0ac16b4ad2e370
oif <neowiz@icloud.com>
2017-10-24 22:26:34 +0800

    MOD: Fixed plugins bug

--- plugin/cache/item.go
+++ plugin/cache/item.go
@@ -3,21 +3,29 @@ package cache
 import "github.com/miekg/dns"
 
 type item struct {
-	Rcode              int
-	Authoritative      bool
-	AuthenticatedData  bool
+	RecursionDesired   bool
 	RecursionAvailable bool
+	AuthenticatedData  bool
+	CheckingDisabled   bool
+	Rcode              int
 	Answer             []dns.RR
 	Ns                 []dns.RR
 	Extra              []dns.RR
+
+	originTTL uint32
+	storedAt  uint32
 }
 
 func newItem(m *dns.Msg) *item {
 	i := new(item)
 	i.Rcode = m.Rcode
-	i.Authoritative = m.Authoritative
-	i.AuthenticatedData = m.AuthenticatedData
+	i.RecursionDesired = m.RecursionDesired
 	i.RecursionAvailable = m.RecursionAvailable
+	i.AuthenticatedData = m.AuthenticatedData
+	i.CheckingDisabled = m.CheckingDisabled
+
+	// fmt.Printf("ans %v", m.Answer)
+
 	i.Answer = m.Answer
 	i.Ns = m.Ns
 	i.Extra = make([]dns.RR, len(m.Extra))
@@ -36,14 +44,19 @@ func newItem(m *dns.Msg) *item {
 }
 
 func (i *item) replyToMsg(m *dns.Msg) {
-	m.Authoritative = false
-	m.AuthenticatedData = i.AuthenticatedData
-	m.RecursionAvailable = i.RecursionAvailable
-	m.Rcode = i.Rcode
-	m.Compress = true
+	tmp := new(dns.Msg)
+	tmp.SetReply(m)
+	tmp.Compress = true
+	tmp.Authoritative = false
+	tmp.Rcode = i.Rcode
+	// m.RecursionDesired = i.RecursionDesired
+	tmp.RecursionAvailable = i.RecursionAvailable
+	tmp.AuthenticatedData = i.AuthenticatedData
+	// m.CheckingDisabled = i.CheckingDisabled
 
-	m.Answer = i.Answer
-	m.Ns = i.Ns
-	m.Extra = i.Extra
+	tmp.Answer = i.Answer
+	tmp.Ns = i.Ns
+	tmp.Extra = i.Extra
 
+	tmp.CopyTo(m)
 }
