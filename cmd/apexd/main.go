b1cdba7366606e5623c6fc8be94337e0e995d5fb
Neo Zhuo <neowiz@icloud.com>
2018-07-21 15:13:50 +0800

    MOD: Modify main

--- cmd/apexd/main.go
+++ cmd/apexd/main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"github.com/oif/apex/pkg/config"
 	engine "github.com/oif/apex/pkg/engine/v1"
 	"github.com/oif/apex/plugin/cache"
 	"github.com/oif/apex/plugin/gdns"
-	"github.com/oif/apex/plugin/hosts"
 	"github.com/oif/apex/plugin/mupstream"
 	"github.com/oif/apex/plugin/statistics"
 
@@ -13,7 +11,7 @@ import (
 )
 
 func main() {
-	log.SetLevel(log.WarnLevel)
+	log.SetLevel(log.DebugLevel)
 	s := new(engine.Server)
 	s.ListenAddress = ":53"
 	s.ListenProtocol = []string{"udp"}
@@ -22,28 +20,19 @@ func main() {
 		plugin.ConfigFilePath = "statistics.toml"
 		return plugin
 	}())
-	s.RegisterPlugins(func() *hosts.Plugin {
-		plugin := new(hosts.Plugin)
-		plugin.Config = config.Hosts{
-			Enable:         true,
-			UpdateInterval: 3600,
-			Address:        "https://raw.githubusercontent.com/racaljk/hosts/master/hosts",
-		}
-		return plugin
-	}())
 	s.RegisterPlugins(func() *cache.Plugin {
 		plugin := new(cache.Plugin)
 		plugin.CacheSize = 1024
-		return plugin
-	}())
-	s.RegisterPlugins(func() *mupstream.Plugin {
-		plugin := new(mupstream.Plugin)
 		return plugin
 	}())
 	s.RegisterPlugins(func() *gdns.Plugin {
 		plugin := new(gdns.Plugin)
 		plugin.EnableProxy = true
 		plugin.ProxyAddr = "http://127.0.0.1:8118"
+		return plugin
+	}())
+	s.RegisterPlugins(func() *mupstream.Plugin {
+		plugin := new(mupstream.Plugin)
 		return plugin
 	}())
 	s.Run()
