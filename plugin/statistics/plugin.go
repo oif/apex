99b4d5c9996b7acb2e2bc68b525a7615241f54a7
Neo Zhuo <neowiz@icloud.com>
2018-07-21 15:15:10 +0800

    MOD: Update statistics plugin

--- plugin/statistics/plugin.go
+++ plugin/statistics/plugin.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/Sirupsen/logrus"
 	client "github.com/influxdata/influxdb/client/v2"
+	"net/http"
 )
 
 var influxdbClient client.Client
@@ -19,8 +20,10 @@ const PluginName = "Statistics Plugin"
 type Plugin struct {
 	ConfigFilePath string
 
-	points    []*client.Point
-	writeLock sync.Mutex
+	points      []*client.Point
+	countBuffer int64
+	writeLock   sync.Mutex
+	httpClient  *http.Client
 }
 
 // Name return the name of this plugin
@@ -41,6 +44,10 @@ func (p *Plugin) Initialize() error {
 	if err != nil {
 		return err
 	}
+
+	p.httpClient = &http.Client{
+		Timeout: 3 * time.Second,
+	}
 	// cron job
 	go func() {
 		for {
@@ -59,19 +66,24 @@ func (p *Plugin) Warmup(c *plugin.Context) {
 // AfterResponse implements plugin
 func (p *Plugin) AfterResponse(c *plugin.Context, err error) {
 	responseTime := makeNanoTimestamp() - c.GetInt64("statistics_plugin:startTime")
+	hitCache := c.GetBool("cache_plugin:write")
 	c.Logger().WithFields(logrus.Fields{
 		"response_time": responseTime,
+		"hit_cache":     hitCache,
 	}).Info("Response time usage statistics")
 	// write influxdb
-	go func(responseTime int64) {
+	go func(responseTime int64, hitCache bool) {
 		if len(c.Msg.Question) < 1 {
 			return
 		}
-		err := p.pushResponsePoint(c.Msg.Question[0].Qtype, c.Msg.Question[0].Name, responseTime, !c.HasError())
+		// count resolve
+		p.resolveCount()
+
+		err := p.pushResponsePoint(c.Msg.Question[0].Qtype, c.Msg.Question[0].Name, responseTime, !c.HasError(), hitCache)
 		if err != nil {
 			logrus.Error(err)
 		}
-	}(responseTime)
+	}(responseTime, hitCache)
 }
 
 // Patch the dns pakcage
@@ -81,7 +93,7 @@ func makeNanoTimestamp() int64 {
 	return time.Now().UnixNano() / int64(time.Millisecond)
 }
 
-func (p *Plugin) pushResponsePoint(qtype uint16, name string, responseTime int64, isSuccess bool) (err error) {
+func (p *Plugin) pushResponsePoint(qtype uint16, name string, responseTime int64, isSuccess, hitCache bool) (err error) {
 	pt, err := client.NewPoint(
 		"dns_query",
 		map[string]string{
@@ -91,6 +103,7 @@ func (p *Plugin) pushResponsePoint(qtype uint16, name string, responseTime int64
 		map[string]interface{}{
 			"response_time": responseTime,
 			"success":       isSuccess,
+			"hit_cache":     hitCache,
 		},
 		time.Now(),
 	)
@@ -120,6 +133,13 @@ func (p *Plugin) writeResponse() {
 	p.writeLock.Unlock()
 
 	err = influxdbClient.Write(bp) // ignore error
+	if err != nil {
+		logrus.Error(err)
+	}
+}
+
+func (p *Plugin) resolveCount() {
+        _, err := p.httpClient.Get("FIXME")
 	if err != nil {
 		logrus.Error(err)
 	}
